2672d79c2c0e708bfc73dbe952362ab3bf6a5a56
NishidaRyu416 <nishidaryu416@gmail.com>
2017-08-17 16:15:41 +0900

    repair

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in?,only:[:show]
+
   def show
     @user=User.find(params[:id])
+  end
+
+  private
+
+  def logged_in?
+    redirect_to root_path unless current_user
   end
 end
